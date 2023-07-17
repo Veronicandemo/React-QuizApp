5e7d8a641f83807ac1c4112128ce3ae8cdb908dc
Veronicandemo <ndemoveronica@gmail.com>
2023-07-18 01:15:40 +0300

    React quiz app

---
+++ src/components/Progress.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Progress = ({ index, numQuestions, points, maxPoints, answer }) => {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question{' '}
+        <strong>
+          {index + 1} / {numQuestions}
+        </strong>
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPoints}
+      </p>
+    </header>
+  )
+}
+
+export default Progress
